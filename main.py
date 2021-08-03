2f6e3c4b03ad8df51016aa811abb77310623a7be
Drew Bayles <dtbayles@gmail.com>
2021-08-02 22:36:23 -0600

    Added account and market hour endpoints

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
 import requests
-import secrets
-#from config import CLIENT_ID
-
 import urllib.parse
+import json
+from secrets import access_token, redirect_uri, client_id, refresh_token
 
-refresh_token = {
-  "access_token": secrets.access_token,
-  "refresh_token": secrets.refresh_token,
-  "scope": "PlaceTrades AccountAccess MoveMoney",
-  "expires_in": 1800,
-  "refresh_token_expires_in": 7776000,
-  "token_type": "Bearer"
-}
+'''
+How to generate a new refresh token if old one has completely expired (older than 90 days)
+'''
+def generate_refresh_token():
+    auth_url = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=" + urllib.parse.quote(redirect_uri) + "&client_id=" + urllib.parse.quote(client_id) + "%40AMER.OAUTHAP"
+    print(auth_url)
+    '''
+    1. Sign in and then URL decode the code --> urllib.parse.unquote(CODE)
+        print(urllib.parse.unquote(CODE))
+    2. Enter the following request data online: https://developer.tdameritrade.com/authentication/apis/post/token-0
+        reqData = {
+            'grant_type': 'authorization_code',
+            'access_type': 'offline',
+            'code': urllib.parse.unquote("CODE"),
+            'client_id': secrets.client_id,
+            'redirect_uri': secrets.redirect_uri
+        }
+    3. Save the refresh_token and access_token
+    '''
 
-print(urllib.parse.quote(secrets.redirect_url))
-print(urllib.parse.quote(secrets.client_id))
+'''
+To be used to obtain a 30 minute access_token
+'''
+def refresh_access_token():
+    auth_url = "https://api.tdameritrade.com/v1/oauth2/token"
+    reqData = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': client_id,
+    }
+    response = requests.post(auth_url, reqData)
+    print(response.content)
 
-# https://localhost:8080/?code=
+'''
+Fetches and prints a Json of all account details
+'''
+def get_accounts():
+    endpoint = "https://api.tdameritrade.com/v1/accounts"
+    headers = {'Authorization': "Bearer {}".format(access_token)}
+    response = requests.get(endpoint, headers=headers)
+    print(json.dumps(response.json(), indent=4))
 
+def US_market_hours():
+    endpoint = "https://api.tdameritrade.com/v1/marketdata/hours"
+    headers = {'Authorization': "Bearer {}".format(access_token)}
+    parameters = {
+        'markets': 'EQUITY',
+        'date': '2021-08-03'
+    }
+    response = requests.get(endpoint, headers=headers, params=parameters)
+    print(json.dumps(response.json(), indent=4))
 
-print(urllib.parse.unquote(secrets.code))
-
-# https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=http%3A//localhost%3A8080&client_id=3BQTAZGGRDEB1YIXPFBGRQPKL081SOSB%40AMER.OAUTHAP
-
-reqData = {
-    'grant_type': 'authorization_code',
-    'access_type': 'offline',
-    'client_id': secrets.client_id,
-    'redirect_uri': secrets.redirect_url
-}
-
-# response = requests.post("https://api.tdameritrade.com/v1/oauth2/token", reqData)
-
-# print(response)
-
-def login(Consumer_Key):
-    print("login function")
+US_market_hours()
